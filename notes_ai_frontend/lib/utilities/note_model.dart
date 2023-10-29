053c8dd3b30e25f8152c8e2e130bedfc0b9fa557
David Houle-Tymeczko <david.houletymeczko@ontariotechu.net>
2023-10-29 04:01:52 -0400

    Add Note model class and note_model.dart to provide local storage of notes. Very early version of the storage system and will require a method for encoding text and drawings as JSON.:

---
+++ notes_ai_frontend/lib/utilities/note_model.dart
@@ -0,0 +1,47 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import '../models/note.dart';
+
+class NotesModel {
+  static final NotesModel _notesModel = NotesModel._();
+
+  NotesModel._();
+
+  late Database db;
+
+  factory NotesModel() {
+    return _notesModel;
+  }
+
+  Future<void> initDB() async {
+    db = await openDatabase(join(await getDatabasesPath(), 'notes.db'),
+        onCreate: (database, version) async {
+          await database.execute("""CREATE TABLE notes(
+          id INTEGER PRIMARY KEY AUTOINCREMENT, 
+          title VARCHAR(64) NOT NULL, 
+          text TEXT NOT NULL
+          )""");
+        }, version: 1);
+  }
+
+  Future<List<Note>> getAllNotes() async {
+    final List noteMaps = await db.query("notes");
+    return noteMaps.map((e) => Note.fromMap(e)).toList();
+  }
+
+  Future<int> insertNote(Note note) async {
+    return await db.insert("notes", note.toMap());
+  }
+
+  Future<int> deleteNote(int id) async {
+    return await db.delete("notes", where: "id = ?", whereArgs: [id]);
+  }
+
+  Future<int> updateNote(Note note) async {
+    Map<String, dynamic> values = note.toMap();
+    values.remove("id");
+    final count = await db
+        .update("notes", values, where: "id = ?", whereArgs: [note.id]);
+    return count;
+  }
+}
