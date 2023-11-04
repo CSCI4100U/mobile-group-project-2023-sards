b38b877345f4b6711cea82baa2093e92e79703f5
Sheida Ebrahimi <sheida1381@hotmail.com>
2023-11-04 18:53:20 -0400

    feat: Add colors, fonts etc

--- frontend/lib/screens/settings_page.dart
+++ frontend/lib/screens/settings_page.dart
@@ -30,12 +30,17 @@ class _SettingsPageState extends State<SettingsPage> {
               );
             },
           ),
-          title: Text("Settings"),
+          title: const Text("Settings", style:
+          TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 25
+            ),
+          ),
         ),
         body: Column(
           mainAxisSize: MainAxisSize.max,
           children: [
-            Padding(padding: EdgeInsetsDirectional.fromSTEB(0, 12, 0, 0),
+            Padding(padding: const EdgeInsetsDirectional.fromSTEB(0, 12, 0, 0),
               child: SwitchListTile.adaptive(
                   value: _notif,
                   onChanged: (bool value) {
@@ -45,14 +50,20 @@ class _SettingsPageState extends State<SettingsPage> {
                     // Add push notif function
                     );
                   },
-                title: Text("Push Notif"),
-                subtitle: Text("Receive Push notifications from our application on a semi regular basis."),
+                title: const Text("Push Notifications",
+                  style:
+                  TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20),
+                ),
+                subtitle: const Text("Receive Push notifications from our application on a semi regular basis."),
                 dense: false,
                 controlAffinity: ListTileControlAffinity.trailing,
-                contentPadding: EdgeInsetsDirectional.fromSTEB(24, 12, 24, 12),
+                contentPadding: const EdgeInsetsDirectional.fromSTEB(24, 12, 24, 12),
+                activeColor: const Color.fromRGBO(249, 207, 88, 100),
               ),
             ),
-            Padding(padding: EdgeInsetsDirectional.fromSTEB(0, 12, 0, 0),
+            Padding(padding: const EdgeInsetsDirectional.fromSTEB(0, 12, 0, 0),
               child: SwitchListTile.adaptive(
                 value: _sync,
                 onChanged: (bool value) {
@@ -62,14 +73,20 @@ class _SettingsPageState extends State<SettingsPage> {
                   // Add Sync function
                   );
                 },
-                title: Text("Sync To Cloud"),
-                subtitle: Text("Automatically sync your notes to cloud once you are done editing it."),
+                title: const Text("Sync To Cloud",
+                  style:
+                  TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20),
+                ),
+                subtitle: const Text("Automatically sync your notes to cloud once you are done editing it."),
                 dense: false,
                 controlAffinity: ListTileControlAffinity.trailing,
-                contentPadding: EdgeInsetsDirectional.fromSTEB(24, 12, 24, 12),
+                contentPadding: const EdgeInsetsDirectional.fromSTEB(24, 12, 24, 12),
+                activeColor: const Color.fromRGBO(249, 207, 88, 100),
               ),
             ),
-            Padding(padding: EdgeInsetsDirectional.fromSTEB(0, 12, 0, 0),
+            Padding(padding: const EdgeInsetsDirectional.fromSTEB(0, 12, 0, 0),
               child: SwitchListTile.adaptive(
                 value: _theme,
                 onChanged: (bool value) {
@@ -79,22 +96,58 @@ class _SettingsPageState extends State<SettingsPage> {
                   // Add switch to light mode function
                   );
                 },
-                title: Text("Light Mode"),
+                title: const Text("Light Mode",
+                  style:
+                  TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20),
+                ),
                 dense: false,
                 controlAffinity: ListTileControlAffinity.trailing,
-                contentPadding: EdgeInsetsDirectional.fromSTEB(24, 12, 24, 12),
+                contentPadding: const EdgeInsetsDirectional.fromSTEB(24, 12, 24, 12),
+                activeColor: const Color.fromRGBO(249, 207, 88, 100),
               ),
             ),
-            Padding(padding: EdgeInsetsDirectional.fromSTEB(0, 24, 0, 0),
+            Padding(padding: const EdgeInsetsDirectional.fromSTEB(0, 24, 0, 0),
               child: ElevatedButton(
                 onPressed: (){}, // Add functionality
-                child: Text("Import From Cloud"),
                 style: ElevatedButton.styleFrom(
                   // Add theme color and font later
-                  fixedSize: Size.fromHeight(50),
+                  fixedSize: const Size.fromHeight(50),
+                  backgroundColor: const Color.fromRGBO(249, 207, 88, 100),
+                ),
+                child: const Text("Import From Cloud",
+                  style:
+                  TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18),
                 ),
               ),
-            )
+            ),
+            const Align(
+              alignment: AlignmentDirectional(-1.00, 1.00),
+              child: Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(12, 30, 24, 12),
+                child: Text(
+                  "App Version",
+                  style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500,)
+                ),
+              ),
+            ),
+            const Align(
+              alignment: AlignmentDirectional(-1.00, 1.00),
+              child: Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(12, 6, 0, 0),
+                child: Text(
+                    "v0.0.1",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,)
+                ),
+              ),
+            ),
           ],
         ),
     );
