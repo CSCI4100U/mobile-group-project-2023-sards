a10e863563586fba65907d5cd0b611609d9f2f6d
Royce Mathew <royce1mathew@gmail.com>
2023-11-09 15:40:54 -0500

    fix: ask voice permission only when needed

--- frontend/lib/screens/create_note.dart
+++ frontend/lib/screens/create_note.dart
@@ -8,12 +8,11 @@ import 'package:speech_to_text/speech_recognition_result.dart';
 import 'package:speech_to_text/speech_to_text.dart';
 import 'package:flutter_tts/flutter_tts.dart';
 
-/// TODO: 
+/// TODO:
 /// Implement text to speech
-/// Cleanup UI 
+/// Cleanup UI
 
 class NoteForm extends StatefulWidget {
-  
   const NoteForm({Key? key, this.noteData}) : super(key: key);
   final Map<String, dynamic>? noteData;
 
@@ -33,15 +32,11 @@ class _NoteFormState extends State<NoteForm> {
   @override
   void initState() {
     super.initState();
-    _titleController = TextEditingController(text: widget.noteData?['title'] ?? "");
-    _textController = TextEditingController(text: widget.noteData?['text'] ?? "");
-    _initSpeech();
+    _titleController =
+        TextEditingController(text: widget.noteData?['title'] ?? "");
+    _textController =
+        TextEditingController(text: widget.noteData?['text'] ?? "");
     _initTts();
-  }
-
-  void _initSpeech() async  {
-    _speechToTextEnabled = await speechToText.initialize(); // init stt only once per app session
-    setState(() {});
   }
 
   @override
@@ -51,18 +46,18 @@ class _NoteFormState extends State<NoteForm> {
     super.dispose();
   }
 
-   // Pick images from platform specific gallery
+  // Pick images from platform specific gallery
   Future pickImageFromGallery() async {
     try {
       final image = await ImagePicker().pickImage(source: ImageSource.gallery);
-      if(image == null) return;
+      if (image == null) return;
       return image.path;
-    } on PlatformException catch(e) {
+    } on PlatformException catch (e) {
       print('Failed to pick image: $e');
     }
   }
 
-  // Populate note content with speech 
+  // Populate note content with speech
   void onSpeechResult(SpeechRecognitionResult result) {
     setState(() {
       isListening = true;
@@ -70,12 +65,12 @@ class _NoteFormState extends State<NoteForm> {
     });
   }
 
-   _initTts() async {
+  _initTts() async {
     flutterTts = FlutterTts();
     await flutterTts.awaitSpeakCompletion(true);
     var engine = await flutterTts.getDefaultEngine;
     var voice = await flutterTts.getDefaultVoice;
-   }
+  }
 
   // speak TTS
   Future _speak(String text) async {
@@ -91,6 +86,55 @@ class _NoteFormState extends State<NoteForm> {
     var result = await flutterTts.stop();
   }
 
+  /*
+    * Listen to speech and append to text field
+    * If speech to text is not enabled, enable it and recurse
+    * If speech to text is enabled, listen to speech
+  */
+  void listenToSpeech(BuildContext context) {
+    if (_speechToTextEnabled) {
+      if (!isListening) {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            content: Row(
+              children: [
+                const Text('Listening'),
+                const SizedBox(width: 0.5),
+                JumpingDots(
+                  color: Colors.black38,
+                  radius: 5,
+                  numberOfDots: 3,
+                  animationDuration: const Duration(milliseconds: 200),
+                )
+              ],
+            ),
+            backgroundColor: const Color.fromRGBO(249, 207, 88, 100)));
+        speechToText.listen(onResult: onSpeechResult);
+      } else {
+        speechToText.stop();
+        setState(() {
+          isListening = false;
+        });
+        ScaffoldMessenger.of(context)
+            .showSnackBar(const SnackBar(content: Text('Stopped Listening')));
+      }
+    } else {
+      speechToText.initialize().then((value) {
+        if (value == true) {
+          setState(() {
+            _speechToTextEnabled = value;
+          });
+          listenToSpeech(context);
+        } else {
+          ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+              content: Text(
+                  'Speech to text is not enabled on this device. Please try again later.')));
+        }
+      }, onError: (stackTrace) {
+        debugPrint('Error initializing speech to text: $stackTrace');
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -98,48 +142,17 @@ class _NoteFormState extends State<NoteForm> {
         title: const Text('Add a Note'),
         actions: [
           IconButton(
-            onPressed: () {
-              if(_speechToTextEnabled) {
-                if(!isListening) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(
-                          content: Row(
-                            children: [
-                              const Text('Listening'),
-                              const SizedBox(width: 0.5),
-                              JumpingDots(color: Colors.black38,
-                              radius: 5,
-                              numberOfDots: 3,
-                              animationDuration: const Duration(milliseconds: 200),
-                              )
-                            ],
-                          ),
-                      backgroundColor: const Color.fromRGBO(249, 207, 88, 100)
-                      )
-                  );
-                  speechToText.listen(
-                    onResult: onSpeechResult
-                  );
-                }
-                else {
-                  speechToText.stop();
-                  setState(() {
-                    isListening = false;
-                  });
-                  ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Stopped Listening')));
-                }
-              }
-            },
-            icon: const Icon(Icons.mic)
-          ),
+              onPressed: () {
+                listenToSpeech(context);
+              },
+              icon: const Icon(Icons.mic)),
           IconButton(
-            onPressed: () async {
-             var imagePath = await pickImageFromGallery();
-             String text = await FlutterTesseractOcr.extractText(imagePath);
-              _textController.text += text;
-            },
-            icon: const Icon(Icons.document_scanner_outlined)
-          )
+              onPressed: () async {
+                var imagePath = await pickImageFromGallery();
+                String text = await FlutterTesseractOcr.extractText(imagePath);
+                _textController.text += text;
+              },
+              icon: const Icon(Icons.document_scanner_outlined))
         ],
       ),
       body: Padding(
@@ -153,20 +166,21 @@ class _NoteFormState extends State<NoteForm> {
                   anchors: editableTextState.contextMenuAnchors,
                   buttonItems: <ContextMenuButtonItem>[
                     ContextMenuButtonItem(
-                      label: 'Text to Speech',
-                      onPressed: () {
-                        _speak(_textController.text);
-                      }
-                    ),
+                        label: 'Text to Speech',
+                        onPressed: () {
+                          _speak(_textController.text);
+                        }),
                     ContextMenuButtonItem(
                       onPressed: () {
-                        editableTextState.copySelection(SelectionChangedCause.toolbar);
+                        editableTextState
+                            .copySelection(SelectionChangedCause.toolbar);
                       },
                       type: ContextMenuButtonType.copy,
                     ),
                     ContextMenuButtonItem(
                       onPressed: () {
-                        editableTextState.selectAll(SelectionChangedCause.toolbar);
+                        editableTextState
+                            .selectAll(SelectionChangedCause.toolbar);
                       },
                       type: ContextMenuButtonType.selectAll,
                     ),
@@ -177,7 +191,6 @@ class _NoteFormState extends State<NoteForm> {
                 labelText: 'Title',
               ),
             ),
-            
             TextField(
               controller: _textController,
               contextMenuBuilder: (context, editableTextState) {
@@ -185,20 +198,21 @@ class _NoteFormState extends State<NoteForm> {
                   anchors: editableTextState.contextMenuAnchors,
                   buttonItems: <ContextMenuButtonItem>[
                     ContextMenuButtonItem(
-                      label: 'Text to Speech',
-                      onPressed: () {
-                        _speak(_textController.text);
-                      }
-                    ),
+                        label: 'Text to Speech',
+                        onPressed: () {
+                          _speak(_textController.text);
+                        }),
                     ContextMenuButtonItem(
                       onPressed: () {
-                        editableTextState.copySelection(SelectionChangedCause.toolbar);
+                        editableTextState
+                            .copySelection(SelectionChangedCause.toolbar);
                       },
                       type: ContextMenuButtonType.copy,
                     ),
                     ContextMenuButtonItem(
                       onPressed: () {
-                        editableTextState.selectAll(SelectionChangedCause.toolbar);
+                        editableTextState
+                            .selectAll(SelectionChangedCause.toolbar);
                       },
                       type: ContextMenuButtonType.selectAll,
                     ),
@@ -213,7 +227,8 @@ class _NoteFormState extends State<NoteForm> {
             ElevatedButton(
               onPressed: () {
                 Navigator.pop(context, {
-                  ...widget.noteData ?? {}, // Append the prefilled data from the widget
+                  ...widget.noteData ??
+                      {}, // Append the prefilled data from the widget
                   'title': _titleController.text,
                   'text': _textController.text,
                 });
@@ -226,4 +241,3 @@ class _NoteFormState extends State<NoteForm> {
     );
   }
 }
-
