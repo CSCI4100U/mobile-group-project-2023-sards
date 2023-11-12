23574dd0cd65bf18453ddd7e9854c3f2661ea973
Royce Mathew <royce1mathew@gmail.com>
2023-11-12 11:12:13 -0500

    optimize: constructor for database helper

--- frontend/lib/services/database_helper.dart
+++ frontend/lib/services/database_helper.dart
@@ -3,36 +3,28 @@ import 'package:sqflite/sqflite.dart';
 import 'package:kanjou/models/note.dart';
 
 class DatabaseHelper {
-  static Future<Database> _init() async {
-    // set up the database
-    String databasesPath = await getDatabasesPath();
-    String dbPath = join(databasesPath, 'notes.db');
+  var db;
 
-    var database =
-        await openDatabase(dbPath, version: 1, onCreate: (db, version) async{
-      await db.execute('''
-        CREATE TABLE notes(
-          id TEXT PRIMARY KEY NOT NULL,
-          title TEXT NOT NULL,
-          text TEXT NOT NULL,
-          date TEXT NOT NULL,
-          tagId TEXT
-        )
-      ''');
-      await db.execute(
-        '''
-        CREATE TABLE tags(
-          id TEXT PRIMARY KEY NOT NULL,
-          name TEXT NOT NULL
-        )
-        '''
-      );
+  DatabaseHelper() {
+    // Set up the database
+    getDatabasesPath().then((databasesPath) {
+      String dbPath = join(databasesPath, 'notes.db');
+
+      openDatabase(dbPath, version: 1, onCreate: (db, version) async {
+        await db.execute('''
+          CREATE TABLE notes(
+            id TEXT PRIMARY KEY NOT NULL,
+            title TEXT NOT NULL,
+            text TEXT NOT NULL,
+            date TEXT NOT NULL,
+            tag TEXT
+          )
+        ''');
+      }).then((database) => {db = database});
     });
-    return database;
   }
 
   Future<List<Note>> getAllNotes() async {
-    final db = await _init();
     final List<Map<String, dynamic>> maps = await db.query('notes');
     return List.generate(maps.length, (i) {
       return Note.fromMap(maps[i]);
@@ -40,18 +32,15 @@ class DatabaseHelper {
   }
 
   Future<int> insertNote(Note note) async {
-    final db = await _init();
     return await db.insert('notes', note.toMap(),
         conflictAlgorithm: ConflictAlgorithm.replace);
   }
 
   Future<int> deleteNote(Note note) async {
-    final db = await _init();
     return await db.delete('notes', where: 'id = ?', whereArgs: [note.id]);
   }
 
   Future<int> updateNote(Note note) async {
-    final db = await _init();
     return await db
         .update('notes', note.toMap(), where: 'id = ?', whereArgs: [note.id]);
   }
